fac332d916407a29b576fd8f700d4f4eb1bb4e82
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-03-11 12:17:06 -0300

    criei classe Report

--- src/main/java/frameworks/ReportFw.java
+++ src/main/java/frameworks/ReportFw.java
@@ -1,5 +1,47 @@
 package frameworks;
 
+import java.io.File;
+import java.io.IOException;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
 public class ReportFw {
 
+	static String path = System.getProperty("user.dir")+File.separator+"reports"+File.separator+"report.html";
+	static ExtentHtmlReporter reporter;
+	static ExtentReports extent;
+	static ExtentTest extentTest;
+	
+	public static void setUpConfigure(String documentTitulo, String reportNome) {
+		reporter = new ExtentHtmlReporter(path);
+		reporter.config().setDocumentTitle(documentTitulo);
+		reporter.config().setReportName(reportNome);
+		reporter.config().setEncoding("utf-8");
+		
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		
+		extent.setSystemInfo("Sistema Operacional", "Windows 10");
+		extent.setSystemInfo("Browser", "Chrome");
+	}
+	
+	public static void setUpStart(String descricao) {
+		extentTest = extent.createTest(descricao);
+	}
+	
+	public static void log(Status status, String mensagem) {
+		extentTest.log(status, mensagem);
+	}
+
+	public static void log(Status status, String mensagem, String imagemPath) throws IOException {
+		extentTest.log(status, mensagem, MediaEntityBuilder.createScreenCaptureFromPath(imagemPath).build());
+	}
+	
+	public static void tearDownFinish() {
+		extent.flush();
+	}
 }
