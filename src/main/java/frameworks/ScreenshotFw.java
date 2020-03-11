476dc0c67fa74252c6d026d3f188642b9588e4a2
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-03-11 12:23:14 -0300

    Criei classe screenshot

--- src/main/java/frameworks/ScreenshotFw.java
+++ src/main/java/frameworks/ScreenshotFw.java
@@ -1,5 +1,19 @@
 package frameworks;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
 public class ScreenshotFw {
 
+	public static String screenshotCapture(WebDriver driver) throws IOException {
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		String path = System.getProperty("user.dir")+File.separator+"screenshots"+File.separator+"ScreenshotFw"+System.nanoTime()+".jpg";
+		FileHandler.copy(src, new File(path));
+		return path;
+	}
 }
