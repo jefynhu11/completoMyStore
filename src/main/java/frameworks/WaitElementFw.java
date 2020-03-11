2a8529d5d31d5f9b77653877d5b968d0829a1ba2
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-03-11 12:28:48 -0300

    Criei classe waitElement

--- src/main/java/frameworks/WaitElementFw.java
+++ src/main/java/frameworks/WaitElementFw.java
@@ -1,5 +1,20 @@
 package frameworks;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class WaitElementFw {
 
+	public static void  visibilityOfElementLocated(WebDriver driver, By by) {
+		WebDriverWait wait = new WebDriverWait(driver, 15);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+	}
+	
+	public static void visibilityOf(WebDriver driver, WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, 15);
+		wait.until(ExpectedConditions.visibilityOf(element));
+	}
 }
