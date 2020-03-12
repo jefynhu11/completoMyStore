e18d32fda8c93350f018efc0684bfe0c9212c11e
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-03-12 14:13:45 -0300

    import report e criei inicio report

--- src/test/java/testcases/PurchaseProduct.java
+++ src/test/java/testcases/PurchaseProduct.java
@@ -1,11 +1,21 @@
 package testcases;
 
+import static frameworks.ReportFw.log;
+import static frameworks.ReportFw.setUpConfigure;
+import static frameworks.ReportFw.setUpStart;
+import static frameworks.ReportFw.tearDownFinish;
+import static frameworks.ScreenshotFw.screenshotCapture;
+
+import java.io.IOException;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.aventstack.extentreports.Status;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import tasks.AuthenticationTask;
@@ -17,6 +27,8 @@ public class PurchaseProduct {
 	
 	@BeforeEach
 	public void setUp() {
+		setUpConfigure("Automaticon Report", "Execution Report");
+		setUpStart("Compra com sucesso");
 		WebDriverManager.chromedriver().setup();
 		this.driver = new ChromeDriver();
 		this.driver.manage().window().maximize();
@@ -27,11 +39,18 @@ public class PurchaseProduct {
 	@ParameterizedTest
 	@CsvFileSource(resources = "/dados.csv")
 	public void test(String email, String password) {
+		log(Status.INFO, "Validacao titulo");
+		if(driver.getTitle().equals("Login - My Store")) {
+			log(Status.PASS, "Validado titulo");
+		} else {
+			log(Status.FAIL, "Nao validado titulo", screenshotCapture(driver));
+		}
 		authenticationTask.account(email, password);
 	}
 	
 	@AfterEach
 	public void tearDown() {
+		tearDownFinish();
 		if(driver!=null) {
 			driver.quit();
 			driver = null;
