e5c0aa7e80f2f510c9e46274434965e88a80bdc5
Jeferson Lopes Eugenio <jeferson.lopes@dbserver.com.br>
2020-03-12 14:11:33 -0300

    Tirei throws IOException e coloquei try

--- src/main/java/frameworks/ScreenshotFw.java
+++ src/main/java/frameworks/ScreenshotFw.java
@@ -10,10 +10,14 @@ import org.openqa.selenium.io.FileHandler;
 
 public class ScreenshotFw {
 
-	public static String screenshotCapture(WebDriver driver) throws IOException {
-		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		String path = System.getProperty("user.dir")+File.separator+"screenshots"+File.separator+"ScreenshotFw"+System.nanoTime()+".jpg";
-		FileHandler.copy(src, new File(path));
-		return path;
+	public static String screenshotCapture(WebDriver driver) {
+		try {
+			File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+			String path = System.getProperty("user.dir")+File.separator+"screenshots"+File.separator+"ScreenshotFw"+System.nanoTime()+".jpg";
+			FileHandler.copy(src, new File(path));
+			return path;
+		} catch (IOException e) {
+			return "Erro capturar de tela";
+		}
 	}
 }
