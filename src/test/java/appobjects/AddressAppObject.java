d2a8647652bba1821b355c8a5a703c67c994a65b
jefynhu11 <jefyx1@hotmail.com>
2020-03-17 11:43:06 -0300

    coloquei cadas elementos

--- src/test/java/appobjects/AddressAppObject.java
+++ src/test/java/appobjects/AddressAppObject.java
@@ -1,7 +1,46 @@
 package appobjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class AddressAppObject {
 
+	private WebDriver driver;
 	
+	public AddressAppObject(WebDriver driver) {
+		this.driver = driver;
+	}
 	
+	public WebElement getNamePageLabel() {
+		return driver.findElement(By.cssSelector("h1.page-heading"));
+	}
+	
+	public WebElement getFirstNameLastNameLabel() {
+		return driver.findElement(By.cssSelector("#address_delivery li.address_firstname"));
+	}
+	
+	public WebElement getAddressAdressLabel() {
+		return driver.findElement(By.cssSelector("#address_delivery li.address_address1"));
+	}
+	
+	public WebElement getAddressCityCodeLabel() {
+		return driver.findElement(By.cssSelector("#address_delivery li.address_city"));
+	}
+	
+	public WebElement getAddressCountryLabel() {
+		return driver.findElement(By.cssSelector("#address_delivery li.address_country_name"));
+	}
+	
+	public WebElement getAddressPhoneLabel() {
+		return driver.findElement(By.cssSelector("#address_delivery li.address_phone_mobile"));
+	}
+	
+	public WebElement getMessagemTextArea() {
+		return driver.findElement(By.cssSelector("textarea.form-control"));
+	}
+	
+	public WebElement getProceedToCheckoutButton() {
+		return driver.findElement(By.name("processAddress"));
+	}
 }
